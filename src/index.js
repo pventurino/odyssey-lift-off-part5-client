f3f7e98df2657f150184e1232423c109a0e50a86
Pablo Venturino <pventurino@salesforce.com>
2021-12-14 22:19:15 -0800

    Use https://server-catstronauts-pv.herokuapp.com/

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: process.env.APOLLO_SERVER_URI || 'http://localhost:4000',
+  uri: 'https://server-catstronauts-pv.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
